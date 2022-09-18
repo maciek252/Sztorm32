728625aca4a0e97c91a6d7be0c8a7afe381dd1c2
Maciej Szreter <maciej.szreter@gmail.com>
2022-09-18 22:24:26 +0200

    Continuing with the arduino script

--- Sztorm32-Lite/sztorm32_esp32.ino/sztorm32_esp32.ino.ino
+++ Sztorm32-Lite/sztorm32_esp32.ino/sztorm32_esp32.ino.ino
@@ -21,6 +21,12 @@
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
+#include "uCRC16XModemLib.h"
+
+#include "Crc16.h"
+//Crc 16 library (XModem)
+Crc16 crc2; 
+
 // diody dzialaja bez power on!
 // 19 - power on! // chyba nie...
 // 27 - RED LED FRONT 
@@ -34,10 +40,24 @@
 #define LED_BUILTIN 33
 #define LED_BUILTIN2 25
 
+//0xA5, 0x5A, 0x02 , 0x0D , 0x03 , 0x16 , 0xBC , 0x02 , 0x07 , 0xBB
+//0xA5, 0x5A, 0x02 , 0x0D , 0x03 , 0x16 ,0x58 ,0x02 ,0x71 ,0x67
+//A5 5A 00 11 05 FF 2D 00 00 00 7C 98 
+//const unsigned char command[] = {0xA5, 0x5A, 0x02 , 0x0D , 0x03 , 0x16 ,0x58 ,0x02 ,0x71 ,0x67}; 
+const char commandShort[] = {0x00 , 0x11 , 0x05 , 0xff ,0x2d ,0x00, 0x00,0x00}; 
+const char command[] = {0xA5, 0x5A, 0x00 , 0x11 , 0x05 , 0xff ,0x2d ,0x00, 0x00,0x00 ,0x7c ,0x98}; 
+#define RXD2 16
+#define TXD2 17
+
+int sendCounter = 0;
+ 
+HardwareSerial gps_serial(2);
+uCRC16XModemLib crc;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-
+  gps_serial.begin(115200, SERIAL_8N1, RXD2, TXD2);
   pinMode(32, OUTPUT);
   digitalWrite(32, HIGH);   // turn the LED on (HIGH is the voltage level)
 
@@ -53,15 +73,34 @@ void setup() {
   pinMode(18, OUTPUT);
   digitalWrite(18, HIGH);   // turn the LED on (HIGH is the voltage level)
   Serial.begin(115200);
+
+
+  sendCounter = 0;
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  Serial.println("Hello World!");
+  
+  
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN2, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN2, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  if(sendCounter<100){
+    sendCounter++;
+    gps_serial.write( (uint8_t*)command, sizeof(command) );
+  }
+  crc2.clearCrc();
+  for(int j = 0; j < sizeof(commandShort); j++){
+    crc2.updateCrc(commandShort[j]);    
+  }
+  unsigned short value = crc2.getCrc();
+    Serial.print("crc = 0x");
+    Serial.println(value, HEX);
+  
+  uint16_t i = crc.calculate(commandShort, 2);
+  Serial.println(i, HEX);
+  
 }
